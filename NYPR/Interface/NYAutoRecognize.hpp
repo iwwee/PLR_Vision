864bec397e76f2144794f1f4be7ad611e5f32cc9
NathanYu1124 <yusen1124@gmail.com>
2018-06-20 23:41:54 +0800

    更改了目录结构

---
+++ NYPR/Interface/NYAutoRecognize.hpp
@@ -0,0 +1,63 @@
+//
+//  NYAutoRecognize.hpp
+//  NYPlateRecognition
+//
+//  Created by NathanYu on 07/02/2018.
+//  Copyright © 2018 NathanYu. All rights reserved.
+//
+
+#ifndef NYAutoRecognize_hpp
+#define NYAutoRecognize_hpp
+
+#include <stdio.h>
+#include "NYCharacterRecognition.hpp"
+#include "NYPlateDetect.hpp"
+#include "Utils.hpp"
+#include "CacheQueue.hpp"
+#include <queue>
+
+#define CAR_PATH "/Users/NathanYu/Downloads/PlateRecognition/Cars/"
+#define PLATE_OUTPUT_PATH "/Users/NathanYu/Desktop/plates/"
+
+
+
+class NYAutoRecognize {
+    
+public:
+        
+    bool isAnalysing;   // 是否正在处理视频
+    
+    vector<cv::Rect> preRects;  // 上一帧中处理的矩形区域
+    
+    // 自动识别;图片上的车牌号
+    vector<string> recognizePlateNumber(Mat src, vector<vector<map<string, float>>> &charsScore, vector<string> &colors);
+    
+    vector<NYPlate> recognizePlateNumber(Mat src);
+    
+    vector<NYPlate> recognizeVideoPlate(Mat &src);
+    
+    // 分析视频流
+    void analyseVideo(string videoPath);
+    
+    // 停止视频处理
+    void stopAnalysing();
+    
+    // 批处理所有车牌
+    void handleAllCars();
+    
+    // 绘制车牌
+    void drawLicense(Mat &img, NYPlate plate, int index);
+    
+    // 处理高分辨率图片
+    void scaleHDImage(Mat &src);
+    
+    // 轮廓按面积降序排序，去除小轮廓目标
+    bool descSort(vector<cv::Point> p1, vector<cv::Point> p2);
+    
+    // 是否为同一个区域
+    bool isSameRegion(cv::Rect r1, cv::Rect r2);
+    
+};
+
+
+#endif /* NYAutoRecognize_hpp */
---
+++ PLR_Vision/Controller/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  PlateLicenseRecognition
+//
+//  Created by NathanYu on 2018/4/11.
+//  Copyright © 2018 NathanYu. All rights reserved.
+//
+
+import Cocoa
+
+class AboutViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
